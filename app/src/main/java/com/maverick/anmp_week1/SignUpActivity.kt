5e46f3697ca02cdd8fb1ff53cd9049d0d48771b0
Pramodia Suka <71862264+pramodiaSuka@users.noreply.github.com>
2024-02-22 14:28:18 +0700

    Edit sign up

--- app/src/main/java/com/maverick/anmp_week1/SignUpActivity.kt
+++ app/src/main/java/com/maverick/anmp_week1/SignUpActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // SIGN UP FOR NEW USER
+        //1. Cek existing account
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
