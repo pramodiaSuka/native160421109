25c137de3b824a4b481a79badd46f110618997a4
Pramodia Suka <71862264+pramodiaSuka@users.noreply.github.com>
2024-03-06 09:37:25 +0700

    Completed addition fame basic feature

--- app/src/main/java/com/maverick/anmp_week1/GameFragment.kt
+++ app/src/main/java/com/maverick/anmp_week1/GameFragment.kt
@@ -9,8 +9,9 @@ import androidx.navigation.Navigation
 import com.maverick.anmp_week1.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
-
+    private var totalScore: Int = 0
     private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,17 +21,40 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    fun displayNumbers(){
+        binding.firstNumText.text = ((1..99).random()).toString()
+        binding.secondNumText.text = ((1..99).random()).toString()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         if(arguments != null){
             val name = GameFragmentArgs.fromBundle(requireArguments()).playerName
             binding.txtTurn.text = "$name's turn"
+            displayNumbers()
         }
 
-        binding.btnBack.setOnClickListener {
-            val action = GameFragmentDirections.actionMainFragment()
-            Navigation.findNavController(it).navigate(action)
+        binding.btnSubmit.setOnClickListener {
+            var firstNum = binding.firstNumText.text.toString().toInt()
+            var secondNum = binding.secondNumText.text.toString().toInt()
+            var answer = firstNum + secondNum
+            var playerAnswer = binding.answerText.text.toString()
+            if (playerAnswer != "" && answer == playerAnswer.toInt()){
+                totalScore += 1
+                binding.answerText.text?.clear()
+                displayNumbers()
+            }
+            else {
+                val action = GameFragmentDirections.actionResultFragment(totalScore)
+                Navigation.findNavController(it).navigate(action)
+            }
+
         }
+
+//        binding.btnBack.setOnClickListener {
+//            val action = GameFragmentDirections.actionMainFragment()
+//            Navigation.findNavController(it).navigate(action)
+//        }
     }
 }
