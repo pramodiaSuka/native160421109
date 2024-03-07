a16b379b4606a1f03021fce87744812ab71b92c1
Pramodia Suka <71862264+pramodiaSuka@users.noreply.github.com>
2024-03-07 15:03:19 +0700

    Completed Bottom Nav and Drawer (Week 3)

--- app/src/main/java/com/maverick/anmp_week1/MainActivity.kt
+++ app/src/main/java/com/maverick/anmp_week1/MainActivity.kt
@@ -5,20 +5,30 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.maverick.anmp_week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //set up navigation controller
         navController = (supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController) //Menyuruh agar navController menghandle navigation view(yang sembunyi)
+
+        binding.bottomNav.setupWithNavController(navController) //Menyerahkan agar navController menghanddle bottom bar
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         //navigate ke urutan backstack sebelumnya
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, binding.drawerLayout) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/maverick/anmp_week1/MainFragment.kt
+++ app/src/main/java/com/maverick/anmp_week1/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.Navigation
 import com.maverick.anmp_week1.databinding.FragmentMainBinding
 
@@ -21,6 +22,11 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSetting.setOnClickListener {
+            val action = MainFragmentDirections.actionOptionFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
 
         binding.btnStart.setOnClickListener {
             val name = binding.txtName.text.toString()
